b3450a28b22d7686d0962371cc113b4ac745d002
SuperninjaXII <lunguj665@gmail.com>
2024-11-12 19:00:16 +0000

    deno fmt

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## PAPERNET
 
-papernet is a book downloading site written in go with htmx.it is wriiten for students and everybody alike to use.
+papernet is a book downloading site written in go with htmx.it is wriiten for
+students and everybody alike to use.
 
 ## what it offers is
 
@@ -10,12 +11,10 @@ papernet is a book downloading site written in go with htmx.it is wriiten for st
 
 ## how to run
 
-to run this on your own machine first install go lang
-then
+to run this on your own machine first install go lang then
 
 ```
 git clone --depth 1 https://github.com/SuperninjaXII/papernet.git
-
 ```
 
 then
--- public/js/main.js
+++ public/js/main.js
@@ -1,74 +1,70 @@
-let searchInput = document.querySelector(".search-input")
-let searchBtn = document.querySelector('.search-btn')
-let searchResult =document.querySelector("#suggestions")
+let searchInput = document.querySelector(".search-input");
+let searchBtn = document.querySelector(".search-btn");
+let searchResult = document.querySelector("#suggestions");
 
-let wasBtnClicked = false
+let wasBtnClicked = false;
 
-  
-interaction1=() => {
-    searchBtn.style.display='flex'
-    let tl1 =gsap.timeline()
-    tl1.to(searchInput,{
-      width:"70svw"
-    })
-    tl1.to(searchBtn,{
-      y:2,
-      rotation:"3deg",
-      x:5,
-    })
-    
-}
-interaction2=()=>{
-  let tl2 = gsap.timeline()
-  tl2.to(searchBtn,{
-    scale:0.9
-  })
-  tl2.to(searchBtn,{
-  borderTopRightRadius: "2rem",
-  borderBottomRightRadius: "2rem",
-  })
-
-}
-interaction2Rev=()=>{
-  let tl2rev =gsap.timeline()
-  tl2rev.to(searchBtn,{
-    scale:1
-  })
-  tl2rev.to(searchBtn,{
-    borderRadius:0
-  })
-}
-checkBtnState=()=>{
-  if (wasBtnClicked==false) {
-    interaction2()
-    wasBtnClicked=true
-  } else if (wasBtnClicked=true) {
-    interaction1()
-    wasBtnClicked=false
-  } 
-}
-searchInputEvent=()=>{
-  interaction1()
-  interaction2Rev()
-  
-}
-  searchInput.addEventListener("click",searchInputEvent)
-searchBtn.addEventListener("click",checkBtnState)
+interaction1 = () => {
+  searchBtn.style.display = "flex";
+  let tl1 = gsap.timeline();
+  tl1.to(searchInput, {
+    width: "70svw",
+  });
+  tl1.to(searchBtn, {
+    y: 2,
+    rotation: "3deg",
+    x: 5,
+  });
+};
+interaction2 = () => {
+  let tl2 = gsap.timeline();
+  tl2.to(searchBtn, {
+    scale: 0.9,
+  });
+  tl2.to(searchBtn, {
+    borderTopRightRadius: "2rem",
+    borderBottomRightRadius: "2rem",
+  });
+};
+interaction2Rev = () => {
+  let tl2rev = gsap.timeline();
+  tl2rev.to(searchBtn, {
+    scale: 1,
+  });
+  tl2rev.to(searchBtn, {
+    borderRadius: 0,
+  });
+};
+checkBtnState = () => {
+  if (wasBtnClicked == false) {
+    interaction2();
+    wasBtnClicked = true;
+  } else if (wasBtnClicked = true) {
+    interaction1();
+    wasBtnClicked = false;
+  }
+};
+searchInputEvent = () => {
+  interaction1();
+  interaction2Rev();
+};
+searchInput.addEventListener("click", searchInputEvent);
+searchBtn.addEventListener("click", checkBtnState);
 let suggestionTimeout;
 
 const getSuggestion = () => {
   fetch(`/result?q=${searchInput.value}`, {
-    method: 'POST',
+    method: "POST",
   })
-    .then(response => {
-      const contentType = response.headers.get('content-type');
-      if (contentType.includes('application/json')) {
-        return response.json().then(data => {
-          console.log('JSON data:', JSON.stringify(data));
+    .then((response) => {
+      const contentType = response.headers.get("content-type");
+      if (contentType.includes("application/json")) {
+        return response.json().then((data) => {
+          console.log("JSON data:", JSON.stringify(data));
         });
-      } else if (contentType.includes('text/html')) {
-        return response.text().then(text => {
-          console.log('HTML data:', text);
+      } else if (contentType.includes("text/html")) {
+        return response.text().then((text) => {
+          console.log("HTML data:", text);
           searchResult.style.display = "flex";
           searchResult.innerHTML = "";
           searchResult.innerHTML = `${text}`;
@@ -76,32 +72,32 @@ const getSuggestion = () => {
           resetSuggestionTimeout();
         });
       } else {
-        throw new Error('Unsupported content type: ' + contentType);
+        throw new Error("Unsupported content type: " + contentType);
       }
     })
-    .catch(error => console.error('Error:', error));
+    .catch((error) => console.error("Error:", error));
 };
 
 const suggestionsPopAnimation = () => {
- searchResult.style.display="flex"
-  let li = document.querySelectorAll('#suggestions li');
+  searchResult.style.display = "flex";
+  let li = document.querySelectorAll("#suggestions li");
   gsap.fromTo("#suggestions", {
     width: "10svw",
     height: "10svh",
     x: 4,
     borderRadius: "50svw",
-    background: "pink"
+    background: "pink",
   }, {
     width: "70svw",
     height: "auto",
     duration: 0.31,
     borderRadius: ".5em",
     background: "#121212",
-    x: 5
+    x: 5,
   });
-  
-  li.forEach(element => {
-    element.style.display = 'block';
+
+  li.forEach((element) => {
+    element.style.display = "block";
   });
 };
 
@@ -115,30 +111,30 @@ const resetSuggestionTimeout = () => {
 searchInput.addEventListener("input", getSuggestion);
 
 const NextPage = () => {
-    let main = document.querySelector("main");
-    fetch(`/searchresults?q=${searchInput.value}`, {
-        method: 'POST',
+  let main = document.querySelector("main");
+  fetch(`/searchresults?q=${searchInput.value}`, {
+    method: "POST",
+  })
+    .then((response) => {
+      const contentType = response.headers.get("content-type");
+      if (contentType.includes("application/json")) {
+        return response.json().then((data) => {
+          console.log("JSON data:", JSON.stringify(data));
+        });
+      } else if (contentType.includes("text/html")) {
+        return response.text().then((text) => {
+          main.innerHTML = `${text}`;
+        });
+      } else {
+        throw new Error("Unsupported content type: " + contentType);
+      }
     })
-    .then(response => {
-        const contentType = response.headers.get('content-type');
-        if (contentType.includes('application/json')) {
-            return response.json().then(data => {
-                console.log('JSON data:', JSON.stringify(data));
-            });
-        } else if (contentType.includes('text/html')) {
-            return response.text().then(text => {
-                main.innerHTML = `${text}`;
-            });
-        } else {
-            throw new Error('Unsupported content type: ' + contentType);
-        }
+    .then(() => {
+      // Push new state to the history
+      const newUrl = `/search?q=${searchQuery}`;
+      history.pushState({ searchQuery: searchQuery }, "", newUrl);
     })
-  .then(() => {
-        // Push new state to the history
-        const newUrl = `/search?q=${searchQuery}`;
-        history.pushState({ searchQuery: searchQuery }, '', newUrl);
-    })
-    .catch(error => console.error('Error:', error));
+    .catch((error) => console.error("Error:", error));
 };
 
 searchBtn.addEventListener("click", NextPage);
