297b7852dd102eb4a2623a1a94e510fb5a3516c0
SuperninjaXII <lunguj665@gmail.com>
2024-11-18 06:39:41 +0000

    added a better /cartegory routes

--- routes/homeRoute.go
+++ routes/homeRoute.go
@@ -7,16 +7,17 @@ import (
 )
 
 func Routes(app *fiber.App) {
+	//html routes
 	app.Get("/", controllers.Home)
 	app.Get("/admin", controllers.Admin)
 	app.Get("/search", controllers.SearchPage)
 	app.Post("/SearchResults", controllers.BooksPage)
 	app.Post("/result", controllers.SearchResult)
 	app.Get("/download/:id", controllers.DownloadPage)
-
+	app.Get("/related/:tag", controllers.GetBookByCartegoryBooks)
+	//json
 	app.Get("/books", controllers.GetAllBooks)
 	app.Get("/book/:id", controllers.GetBookByID)
-	app.Get("/related/:tag", controllers.GetBookByCartegoryBooks)
 	app.Post("/search", controllers.SearchBooks)
-
+	app.Get("/category/:tag", controllers.GetBookByCartegory)
 }
